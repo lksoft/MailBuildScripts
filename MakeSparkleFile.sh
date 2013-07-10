48e61cfac2cf71da3fdb3ce15e72e354cd35f2d6
Scott Little <scott@littleknownsoftware.com>
2013-07-10 13:53:01 +0200

    Add a Sparkle packager for MPM.

---
+++ MakeSparkleFile.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#  MakeSparkleFile.sh
+#  Mail Plugin Manager
+#
+#  Created by Scott Little on 10/7/13.
+#  Copyright (c) 2013 Little Known Software. All rights reserved.
+
+if [ $CONFIGURATION != "Release" ]; then
+	echo "Not packaging for Non-Deployment Style"
+fi
+
+export REAL_PRODUCT="$MAIN_PRODUCT_NAME"
+if [[ "$COMPACT_PRODUCT_NAME" == "" ]]; then
+	export COMPACT_PRODUCT="$MAIN_PRODUCT_NAME"
+else
+	export COMPACT_PRODUCT="$COMPACT_PRODUCT_NAME"
+fi
+export MY_RELEASE_FOLDER="$SRCROOT/../Releases"
+export MY_PRODUCT_NEW_NAME="$COMPACT_PRODUCT.New"
+export MY_PRODUCT_VERSIONED_NAME="$COMPACT_PRODUCT.$VERSION_STRING"
+
+echo "Creating the zip file for Sparkle"
+
+cd "$BUILT_PRODUCTS_DIR"
+
+pwd
+
+#   remove old zip file if it exists
+if [ -f "$MY_RELEASE_FOLDER/$MY_PRODUCT_NEW_NAME.tar.bz2" ]; then
+	rm -f "$MY_RELEASE_FOLDER/$MY_PRODUCT_NEW_NAME.tar.bz2"
+fi
+
+#   if the versioned name already exists, use the 'New' name
+if [ -f "$MY_RELEASE_FOLDER/$MY_PRODUCT_VERSIONED_NAME.tar.bz2" ]; then
+	export MY_PRODUCT_VERSIONED_NAME="$MY_PRODUCT_NEW_NAME"
+fi
+
+echo "Tarring and zipping File"
+
+#   zip the file into new location
+tar -cyf "$MY_RELEASE_FOLDER/$MY_PRODUCT_VERSIONED_NAME.tar.bz2" "$REAL_PRODUCT.app"
+
