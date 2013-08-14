48be31b527604f907f0afd97cf3783e13730b922
Scott Little <scott@littleknownsoftware.com>
2013-08-14 15:34:02 +0200

    Update to use different USER variable.

--- SetInstallHelperInfoPlist.pl
+++ SetInstallHelperInfoPlist.pl
@@ -5,7 +5,7 @@ use strict;
 die "$0: Must be run from Xcode" unless $ENV{"BUILT_PRODUCTS_DIR"};
 
 # Ensure that the proper values have been setup
-die "No installation sub id found for project [REPLACESUBID]" unless $ENV{"REPLACESUBID"};
+die "No installation sub id found for project [INSTALLER_SUB_ID]" unless $ENV{"INSTALLER_SUB_ID"};
 die "No Codesign identity found" unless $ENV{"CODE_SIGN_IDENTITY"};
 die "The Root Helper folder was not found [COPY_MOVE_DIR]" unless $ENV{"COPY_MOVE_DIR"};
 
@@ -18,7 +18,7 @@ my $INFO_DEST = "$ENV{COPY_MOVE_DIR}/CopyMoveHelperFixed-Info.plist";
 my $info = `plutil -convert xml1 -o - "$INFO_SOURCE"`;
 
 # replace both the branch name and the hash value
-$info =~ s/REPLACESUBID/$ENV{"REPLACESUBID"}/;
+$info =~ s/REPLACESUBID/$ENV{"INSTALLER_SUB_ID"}/;
 $info =~ s/CODESIGNID/$ENV{"CODE_SIGN_IDENTITY"}/;
 
 # Rewrite the contents to the file
