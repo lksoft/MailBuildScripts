81503f59349c4c7f6120d27a9bcf258cb78614c2
Scott Little <scott@littleknownsoftware.com>
2013-08-17 13:45:38 +0200

    Remove dependency on a particular env variable being set as the script is now run from the build and not the scheme.

--- SetInstallHelperInfoPlist.pl
+++ SetInstallHelperInfoPlist.pl
@@ -6,12 +6,11 @@ die "$0: Must be run from Xcode" unless $ENV{"BUILT_PRODUCTS_DIR"};
 
 # Ensure that the proper values have been setup
 die "No Codesign identity found" unless $ENV{"CODE_SIGN_IDENTITY"};
-die "The Root Helper folder was not found [COPY_MOVE_DIR]" unless $ENV{"COPY_MOVE_DIR"};
 
 # Get the current git branch and sha hash
 # 	to use them to set the CFBundleVersion value
-my $INFO_SOURCE = "$ENV{COPY_MOVE_DIR}/CopyMoveHelper-Info.plist";
-my $INFO_DEST = "$ENV{COPY_MOVE_DIR}/CopyMoveHelperFixed-Info.plist";
+my $INFO_SOURCE = "$ENV{SRCROOT}/CopyMoveHelper/CopyMoveHelper-Info.plist";
+my $INFO_DEST = "$ENV{SRCROOT}/CopyMoveHelper/CopyMoveHelperFixed-Info.plist";
 
 # Get the contents as an XML format
 my $info = `plutil -convert xml1 -o - "$INFO_SOURCE"`;
