ff04da5398eae153c3c6b5b5771fc8c2348b8193
Scott Little <scott@littleknownsoftware.com>
2014-03-10 10:55:42 +0100

    Finish up changes to the post publishing part of the scripts.

--- PublishPost.pl
+++ PublishPost.pl
@@ -8,14 +8,12 @@
 
 use strict;
 use warnings;
+use feature qw(switch);
 use JSON qw( decode_json );
 
 my $productCode;
 my $productName;
 my $versionString;
-my $postPath = "";
-my $shouldRetweet = 0;
-my $argParseStart = 0;
 
 if ($ENV{"MAIN_PRODUCT_NAME"}) {
 	$productName = $ENV{"MAIN_PRODUCT_NAME"};
@@ -35,92 +33,114 @@ else {	#	For Command Line Testing purposes only!
 	if ($productCode eq "mpm") {
 		$productName = "MailPluginManager";
 	}
-	$argParseStart = 2;
 }
 
-#	Get passed in variables
-my $argCount = scalar @ARGV;
-if ($argCount > $argParseStart) {
-	if ($ARGV[$argParseStart] =~ m/^(yes|no)$/i) {
-		if (lc($ARGV[$argParseStart]) eq "yes") {
-			$shouldRetweet = 1;
-		}
-		$argParseStart++;
-	}
-	if ($argCount > $argParseStart) {
-		$postPath = $ARGV[$argParseStart];
-	}
-}
-
-#	Build the post contents
+#	Get the date
+my $nowDate = `date "+%Y-%m-%d %H:%M:%S"`;
+$nowDate =~ s/^\s+|\s+$//g;
+(my $mappedVersion = $versionString) =~ s/\./-/g;
+my $postName = lc("$productName-$mappedVersion-released");
 my $postContents = "";
 
+#	Build the post contents
 #	Look for any blog drafts
 my $draftDir = "/Users/scott/Dropbox/PlainText/Blog Drafts/";
-(my $mappedVersion = $versionString) =~ s/\./-/g;
 if (opendir(DIR, $draftDir)) {
 	while (defined(my $aFile = readdir(DIR))) {
-		if ($aFile =~ m/$productName-$mappedVersion-released/i) {
+		if ($aFile =~ m/$postName/i) {
 			$postContents = do {
 				local $/ = undef;
-				open my $fh, "<", $aFile
-					or die "Could not open $aFile for reading: $!";
-				<$fh>;
+				if (open my $fh, "<", ($draftDir . $aFile)) {
+					<$fh>;
+				}
 			};
-			unlink($aFile);
-			break;
+			if ((defined $postContents) && ($postContents ne "")) {
+				unlink($draftDir . $aFile);
+			}
 		}
 	}
 	closedir(DIR);
 }
 
 my $sitePath = "/Users/scott/Sites/lksite/";
-#	If we need to make a new post
+#	If there is no blog draft, then load default content
 if ($postContents eq "") {
-	$postContents = "--- 
-layout: post
-title: \"$productName $versionString released.\"
-date: $nowDate
-categories: [$productName, Release]
----
-We have just released a new version of <span class=\"product-name\">$productName</span> (version $versionString).
-
-For more information, visit the [site](/$productCode) or [download](/download/$productCode) it directly.
-";
-
-	my $versionJSON;
-	my $JSONFileDIR = $sitePath . "/source/services/config/version-info/" . $productCode;
-	# . "/" . $buildNumber . "-" . $versionString . ".json";
-	if (opendir(DIR, $JSONFileDIR)) {
-		while (defined(my $aFile = readdir(DIR))) {
-			if ($aFile =~ m/$versionString\.json$/i) {
-				$versionJSON = do {
-					local $/ = undef;
-					open my $fh, "<", $aFile
-						or die "Could not open $aFile for reading: $!";
-					<$fh>;
-				};
-				break;
-			}
-		}
-		closedir(DIR);
-	}
-	
-	if (defined $versionJSON) {
-		my $decoded = decode_json($versionJSON);
-		my @changeList = @{ $decoded->{"lang"}{"en"}{"changes"} };
-		foreach my $change (@changeList) {
-		}
-	}
+	my $blogTemplatePath = $sitePath . "source/_includes/post/" . $productCode . "_auto.html";
+	$postContents = do {
+		local $/ = undef;
+		open my $fh, "<", $blogTemplatePath
+			or die "Could not open $blogTemplatePath for reading: $!";
+		<$fh>;
+	};
 }
 
+#	Get the release notes into a replacing format
+my $versionJSON;
+my $JSONFileDIR = $sitePath . "/source/services/config/version-info/" . $productCode;
+if (opendir(DIR, $JSONFileDIR)) {
+	while (defined(my $aFile = readdir(DIR))) {
+		if ($aFile =~ m/$versionString\.json$/i) {
+			$versionJSON = do {
+				local $/ = undef;
+				open my $fh, "<", ($JSONFileDIR . "/" . $aFile)
+					or die "Could not open $aFile for reading: $!";
+				<$fh>;
+			};
+		}
+	}
+	closedir(DIR);
+}
+my $changeContents = "";
+if (defined $versionJSON) {
+	$versionJSON =~ s/^\s+|,\s+$//g;
+	my $decoded = decode_json($versionJSON);
+	my @changeList = @{ $decoded->{"lang"}{"en"}{"changes"} };
+	
+	#	Change Mappings
+	my %changeMappings = (
+		"new" => "New",
+		"fix" => "Fixed",
+		"os" => "OS Support",
+		"internal" => "Maintenance"
+	);
+	
+	# Reverse sort file list
+	my @orderedChangeList = sort {
+		my $aTest = 0;
+		my $bTest = 0;
+		given ($a) {
+			when (/new/) { $aTest = 1 }
+			when (/fix/) { $aTest = 2 }
+			when (/os/) { $aTest = 3 }
+			default { $aTest = 4 }
+		}
+		given ($b) {
+			when (/new/) { $bTest = 1 }
+			when (/fix/) { $bTest = 2 }
+			when (/os/) { $bTest = 3 }
+			default { $bTest = 4 }
+		}
+		$aTest cmp $bTest;
+	} @changeList;
+	# Get the contents of the changes into a variable
+	$changeContents = "<p>Release Notes for this version:</p><ul class=\"new-features\">";
+	foreach my $aChange (@orderedChangeList) {
+		$changeContents .= "<li><span class=\"change-type $aChange->{'type'}\">" . $changeMappings{$aChange->{'type'}} . "</span>: $aChange->{'description'}</li>";
+	}
+	$changeContents .= "</ul>";
+}
 
+#	Do the replacement
+$postContents =~ s/__VERSION_STRING__/$versionString/g;
+$postContents =~ s/__NOW_DATE__/$nowDate/g;
+$postContents =~ s/__RELEASE_NOTES_CONTENTS__/$changeContents/;
 
-
-#	Ensure that we really have an authorization for this account
-my $accountInfo = `cd $sitePath;twurl accounts`;
-if ($accountInfo !~ m/$accountName/) {
-	print "Account ‘$accountName’ not found in twurl or twurl not installed.\n";
-	exit 1;
+# Write the contents to the post file
+$nowDate =~ s/^([0-9-]+) .+$/$1/;
+my $postFilePath = $sitePath . "source/_posts/$nowDate-$postName.markdown";
+do {
+	open my $fh, ">", $postFilePath
+		or die "Could not open file $postFilePath for output: $!";
+	print $fh $postContents;
 }
 
