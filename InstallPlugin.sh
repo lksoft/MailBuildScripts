461b782ca5fef7c3c2630e711c17549b678cb1c0
Scott Little <scott@littleknownsoftware.com>
2014-05-14 15:29:56 +0200

    Update the InstallPlugin script to more intelligently install on my system and provide for installing into VMs automatically.

--- InstallPlugin.sh
+++ InstallPlugin.sh
@@ -9,26 +9,53 @@
 # run script to copy the built bundle into the Install directory
 #	but only if it is a development build
 
-echo 'Action is: '$ACTION
-
-echo 'Copying plugin to Public directory...'
-
 PUB_DIR="$HOME/Projects/Built"
+PUB_DIR2="$HOME/Projects/BuiltPlugins"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Make sure that the Dropbox folder is there
-if [ -d $PUB_DIR ]; then
+FULL_PRODUCT="$PRODUCT_NAME.$WRAPPER_EXTENSION"
+SOURCE_PATH="$BUILT_PRODUCTS_DIR/$FULL_PRODUCT"
+
+if [ ! -d "$SOURCE_PATH" ]; then
+	echo "No plugin to install"
+	exit 0
+fi
+
+echo 'Copying plugin to Public directories...'
+
+# Make sure that the Shared folders are there
+if [ -d "$PUB_DIR" ]; then
+
+	echo "Copying to $PUB_DIR"
 
 	# delete any previous version
-	rm -Rf "$PUB_DIR/$PRODUCT_NAME.$WRAPPER_EXTENSION"
+	rm -Rf "$PUB_DIR/$FULL_PRODUCT"
 
 	# copy the bundle
-	cp -Rf "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.$WRAPPER_EXTENSION" "$PUB_DIR"
+	cp -Rf "$SOURCE_PATH" "$PUB_DIR"
 
 fi
 
+# Make sure that the Shared folders are there
+if [ -d "$PUB_DIR2" ]; then
 
-if [ $CONFIGURATION == "Release" ]; then
+	echo "Copying to $PUB_DIR2"
+
+	# delete any previous version
+	rm -Rf "$PUB_DIR2/$FULL_PRODUCT"
+
+	# copy the bundle
+	cp -Rf "$SOURCE_PATH" "$PUB_DIR2"
+
+fi
+
+if [ "$CONFIGURATION" == "Release" ]; then
+	exit 0
+fi
+
+# Don't do anything unless SJL's test environment is running
+if [ ! -d "$HOME/Library/Mail/V2/IMAP-lksofttest@imap.gmail.com" ]; then
+	echo "Not running test environment, won't replace plugin"
 	exit 0
 fi
 
@@ -40,11 +67,12 @@ if [ $MYVAR == "-1" ]; then
 fi
 
 echo 'Deleting any old copy of plugin...'
+DEST_PATH="$HOME/Library/Mail/Bundles/$FULL_PRODUCT"
 
 # delete any previous version
-rm -Rf "$INSTALL_PATH/$PRODUCT_NAME.$WRAPPER_EXTENSION"
+rm -Rf "$DEST_PATH"
 
 echo 'Copying just built plugin...'
 
 # copy the bundle
-cp -Rf "$BUILT_PRODUCTS_DIR/$PRODUCT_NAME.$WRAPPER_EXTENSION" "$INSTALL_PATH"
+cp -Rf "$SOURCE_PATH" "$DEST_PATH"
