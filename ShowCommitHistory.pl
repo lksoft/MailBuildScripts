28270a7d815c0abcc8cf73ada020c650792e05dd
Scott Little <scott@littleknownsoftware.com>
2014-07-07 14:23:47 +0200

    Make additional script to just show the commit comments that would go into a build for testing.

---
+++ ShowCommitHistory.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+#  ShowCommitHistory.pl
+#  Tealeaves
+#
+#  Created by Scott Little on 4/3/2014.
+#  Copyright (c) 2014 Little Known Software. All rights reserved.
+
+use strict;
+use Cwd qw/abs_path/;
+
+my $repoDirectory = $ARGV[0];
+
+my $versionFileContents = "";
+my $previousAndCurrentTags = `cd "$repoDirectory";git describe --tags \`cd "$repoDirectory";git rev-list --tags --abbrev=0 --max-count=2\` --abbrev=0`;
+(my $previousTag = $previousAndCurrentTags) =~ s/^.+\n(.+)\s+/$1/g;
+(my $currentTag = $previousAndCurrentTags) =~ s/^(.+)\n(.+)\s+/$1/g;
+my $commitHistory = `cd "$repoDirectory";git log $previousTag..$currentTag --pretty=format:"%s"`;
+my $commitPattern = "^\\[(new|os|fix)\\]([\\s-]*)(.+)\$";
+my $startLine = "\n";
+foreach my $aLine (split /\n/, $commitHistory) {
+	if ($aLine =~ m/$commitPattern/i) {
+		my @values = $aLine =~ m/$commitPattern/i;
+		$versionFileContents .= $startLine . '[' . lc($values[0]) . ']:"' . $values[2] . '"';
+		$startLine = "\n";
+	}
+}
+
+print $versionFileContents;
+print "\n\n";
