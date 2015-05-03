12cd4e6b3684d0498e0ab13f3973718c54076349
Scott Little <scott@littleknownsoftware.com>
2015-05-03 23:22:28 +0200

    Add link to Indev Slack

--- PostURLToSlack.sh
+++ PostURLToSlack.sh
@@ -7,5 +7,8 @@ fi
 
 VERSION=`cat "$TEMP_VERSION_STRING_PATH"`
 
-echo "Posting build URL to Slack"
+echo "Posting build URL to LKS Slack"
 curl -X POST "https://littleknown.slack.com/services/hooks/slackbot?token=KIaU8DnZuVhsjIjNTU3ShIRx&channel=%23builds" -d "New Build of $MAIN_PRODUCT_NAME $VERSION ($1): http://media.littleknownsoftware.com/$PRODUCT_CODE/$MAIN_PRODUCT_NAME.$VERSION.dmg" -m 30 -s
+
+echo "Posting build URL to LKS Slack"
+curl -X POST "https://indev.slack.com/services/hooks/slackbot?token=Clnt0J7gPsIWnkpkGwJRN7vw&channel=%23newbuilds" -d "New $1 *LKS Build* of $MAIN_PRODUCT_NAME $VERSION : http://media.littleknownsoftware.com/$PRODUCT_CODE/$MAIN_PRODUCT_NAME.$VERSION.dmg" -m 30 -s
