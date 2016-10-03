5cc9b997d8dc46e754ab9f145bfbc607e5bb89f3
Scott Little <scott@littleknownsoftware.com>
2016-10-03 11:04:33 +0200

    Ensure that the compact name is used if there.

--- PostURLToSlack.sh
+++ PostURLToSlack.sh
@@ -23,10 +23,14 @@ elif [[ "$BUILD_TYPE" != "RELEASE" ]]; then
 	exit 1;
 fi
 
+VERSIONED_PRODUCT_NAME="$MAIN_PRODUCT_NAME.$VERSION"
+if [[ "$COMPACT_PRODUCT_NAME" != "" ]]; then
+	VERSIONED_PRODUCT_NAME="$COMPACT_PRODUCT_NAME.$VERSION"
+fi
 
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 GIT_HASH=`git rev-parse --short HEAD`
-MESSAGE_STRING="*$BUILD_TYPE* $MAIN_PRODUCT_NAME build - $VERSION $BUILD_NUM[$GIT_BRANCH/$GIT_HASH]: https://s3.amazonaws.com/media.smallcubed.com/$SUB_PATH/$PRODUCT_CODE/$MAIN_PRODUCT_NAME.$VERSION.dmg"
+MESSAGE_STRING="*$BUILD_TYPE* $MAIN_PRODUCT_NAME build - $VERSION $BUILD_NUM[$GIT_BRANCH/$GIT_HASH]: https://s3.amazonaws.com/media.smallcubed.com/$SUB_PATH/$PRODUCT_CODE/$VERSIONED_PRODUCT_NAME.dmg"
 
 echo "Posting build URL to LKS Slack"
 curl -X POST "https://littleknown.slack.com/services/hooks/slackbot?token=KIaU8DnZuVhsjIjNTU3ShIRx&channel=%23builds" -d "$MESSAGE_STRING" -m 30 -s
