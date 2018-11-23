e83208fd7f5bb34c80eff1f665866bbb02f3fb45
Scott Little <scott@littleknownsoftware.com>
2018-11-23 14:34:33 +0000

    Update script

--- InstallPlugin.sh
+++ InstallPlugin.sh
@@ -25,8 +25,16 @@ if [ "$CONFIGURATION" == Release* ]; then
 	exit 0
 fi
 
+DEST_FOLDER="$HOME/Library/Mail/Bundles"
+
+if [ ! -d "$DEST_FOLDER" ]; then
+	echo "Creating Bundle Folder: ${DEST_FOLDER}"
+	mkdir "$DEST_FOLDER"
+fi
+
+
 echo 'Deleting any old copy of plugin...'
-DEST_PATH="$HOME/Library/Mail/Bundles/$FULL_PRODUCT"
+DEST_PATH="$DEST_FOLDER/$FULL_PRODUCT"
 
 # delete any previous version
 rm -Rf "$DEST_PATH"
